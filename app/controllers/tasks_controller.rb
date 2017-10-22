7aa8cb04aa95bca1b2031bd9e81e755c1540e8c6
Arunkumar V S <vsarunkumar27@gmail.com>
2017-10-22 13:48:49 +0530

    Task CRUD operations

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(task_params)
+      redirect_to(action: 'show', id: @task.id)
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to(action: 'index')
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :project_id, :hour)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
   devise_for :users
 
   resources :projects
+  resources :tasks
 end
